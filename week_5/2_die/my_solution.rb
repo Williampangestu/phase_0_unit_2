b154df6b3e39324dfc2320e97c2e54a37c51ecc6
williampp <pangestu.william@gmail.com>
2014-05-23 01:30:53 -0700

    Added my solution 2

--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -5,38 +5,52 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: array of labels
+# Output: the number of elements in the array and the random elements from the array
 # Steps:
 
+# Inside the initialize method, create a control flow
+# if the inputted parameter sides is an empty array, raise an Argument Error
+# then create an instance variable and set it equal to labels
+# Inside the sides method, return the length of the instance variables from the initialize method
+# Inside the roll method, use the sample method on the instance variables
 
 # 3. Initial Solution
 
 class Die
   def initialize(labels)
+  	if labels.empty?
+  		raise ArgumentError.new("The list of labels can't be empty")
+  	end
+    @sides = labels
   end
 
   def sides
+  	return @sides.length
   end
 
   def roll
+  	return @sides.sample
   end
 end
 
 
 
 # 4. Refactored Solution
-
-
-
+# Like the previous exercise, I also think that my code does not need to be refactored since it is pretty simple and easy to understand.
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+test = Die.new(["a","b","c"])
 
-
-
+p test.sides == 3
+p test.roll == "a" || "b" || "c"
 
 
 
 # 5. Reflection 
+# Since this challenge is similar to the previous one, I didn't have any trouble on completing it. 
+# In addition, after doing it for so many times, I feel like I am getting  a lot more comfortable on testing my code 
+# using the rspec and the driver test. 
+
