8b4d6ec085056a6711ba86a1d7098f13c3bef3e1
williampp <pangestu.william@gmail.com>
2014-06-02 14:12:03 -0700

    Added my solution 2

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -12,53 +12,60 @@ attr_reader :contents
 
 # Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
+	def initialize
+		@contents = []
+		@open = true
+	end
+
+	def open
+		@open = true
+	end
+
+	def close
+		@open = false
+	end 
+
+	def add_item(item)
+		@contents << item
+	end
+
+	def remove_item(item = @contents.pop) #what is `#pop` doing?
+		@contents.delete(item)
+	end
+
+	def dump  # what should this method return?
+		@contents = []
+		puts "Your drawer is empty."
+	end
+
+	def view_contents
+		puts "The drawer contains:"
+		@contents.each {|silverware| puts "- " + silverware.type }
+	end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+attr_reader :type, :clean
 
 # Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
+	def initialize(type, clean = true)
+		@type = type
+		@clean = clean
+	end
+
+	def eat
+		puts "eating with the #{type}"
+		@clean = false
+	end
+
+	def clean_silverware
+		puts "clean the #{type}"
+		@clean = true
+	end
+
 end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
-end
 
 knife1 = Silverware.new("knife")
 
@@ -85,18 +92,32 @@ silverware_drawer.view_contents
 silverware_drawer.dump
 silverware_drawer.view_contents #What should this return?
 
+fork = Silverware.new("fork") 
+silverware_drawer.add_item(fork) 
+silverware_drawer.view_contents 
 
 fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
 fork.eat
 
 #BONUS SECTION
-# puts fork.clean
+puts fork.clean
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+def assert
+  raise "Assertion failed!" unless yield
+end
 
-
-
-
+assert {silverware_drawer.contents.empty? == true}
+assert {removed_knife.clean == true}
+assert {fork.clean == false}
 
 # 5. Reflection 
+# when doing this challenge, I enjoyed figuring out what each call was doing and debugging them. I also see the ease of using 
+# the assert method to test the code. I'm confident in this challenges Learning Competencies. I was able to identify where the errors 
+# were and I was able to correct them.  
+
+
+
+
+
